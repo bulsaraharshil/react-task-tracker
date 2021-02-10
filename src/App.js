7a9140eb09e7cecf2ce8e95214cc1ebae113c39a
bulsaraharshil <bulsaraharshil@gmail.com>
2021-02-10 20:20:22 +0530

    feat: tasks are getting added and add button is working for form created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import {
-  useState
-} from 'react'
+import {useState} from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 
 function App() {
 
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([{
       id: 1,
       text: "Doctors Appointment",
@@ -27,6 +27,13 @@ function App() {
     }
   ])
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*10000)+1
+    const newTask = {id,...task}
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -42,7 +49,8 @@ function App() {
 
   return (
      <div className = "container" >
-      < Header title = "Task Tracker" / > 
+      < Header onAdd={()=> setShowAddTask(!showAddTask)} showAdd={showAddTask} title = "Task Tracker" / > 
+      {showAddTask && <AddTask onAdd={addTask} />}
       {
         tasks.length > 0 ? ( < Tasks tasks = {
             tasks
